aebe560b7a8a23866faebad20ce05cb2bbc9a97c
Greg Stephen <GregAStephen@gmail.com>
2019-03-21 18:09:04 -0500

    got the expel button to work, alerts saying student is expelled, card goes away

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const startSortBtn = document.getElementById("startSortBtn");
+const sortBtn = document.getElementById("sortBtn");
+const expelBtns = document.getElementsByClassName("expelBtn");
 const firstYearForm = document.getElementById("firstYearForm");
 let studentNameForm = document.getElementById("studentName");
-const sortBtn = document.getElementById("sortBtn");
 const houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
-
 const studentCards = [];
+const expelledStudents = [];
+
 
 //////////////// Event Listeners ///////////////
 
@@ -17,11 +19,21 @@ sortBtn.addEventListener('click', function(e){
     e.preventDefault();
     firstYearForm.style.display = "none";
     let student = createStudentObject(studentNameForm.value);
+    studentNameForm.value = "";
     studentCards.unshift(student);
     console.log(studentCards);
     createStudentCard(studentCards);
-
+    for (const expelBtn of expelBtns) {
+        expelBtn.addEventListener('click', function(e){
+            e.preventDefault();
+            let expelledStudent = this.parentElement.parentElement.id;
+            console.log("expel", this.parentElement.parentElement.id);
+            expel(expelledStudent);
+            firstYearForm.style.display = "block";
+        } )
+    }
 });
+
 
 
 ///////////// FUNCTIONS ////////////
@@ -43,22 +55,34 @@ const createStudentObject = (name) => {
     let house = houses[getRandomNum(3)];
     student.name = name;
     student.house = house;
-    student.isExpelled = false;
     return student;
 };
 
 const createStudentCard = (array) => {
     let domString = '';
     array.forEach(student => {
-        domString += `<div class="card col-3">`;
+        domString += `<div id="${student.name}" class="card col-4">`;
         domString +=    `<div class="card-body">`
         domString +=        `<h3 class="card-title">${student.name}</h3>`;
         domString +=        `<h4 class="card-text">${student.house}</h4>`;
-        domString +=        `<a href="#" class="btn btn-light">Expell</a>`;
+        domString +=        `<a href="#" class="btn btn-light expelBtn">Expel</a>`;
         domString +=    `</div>`;
         domString += `</div>`;
     });
     printToDom("studentCards", domString);
 }
 
+const expel = (studentId) => {
+    console.log("studentId:", studentId )
+    console.log("studentCards:", studentCards);
+    console.log(studentCards.findIndex(x => x.name === `${studentId}`));
+    let expelledStudentIndex = studentCards.findIndex(x => x.name === `${studentId}`);
+    let expelledStudentObject = studentCards.splice(expelledStudentIndex, 1);
+    console.log("ExStObj", expelledStudentObject);
+    expelledStudents.push(expelledStudentObject);
+    console.log("Voldemorts Army", expelledStudents);
+    let expelledStudent = document.getElementById(studentId);
+    expelledStudent.style.display = "none";
+    alert(`"${studentId} has been expelled from Hoggy Hoggy Warts!"`)
 
+}
